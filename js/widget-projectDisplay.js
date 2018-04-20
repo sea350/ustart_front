c4fd00aac0876391b88a88d5bd5b44b38493147d
Kevin Zeng <garudyne@gmail.com>
2018-04-20 14:29:51 -0400

    April 20 Project Modal for User Profile

---
+++ js/widget-projectDisplay.js
@@ -0,0 +1,32 @@
+$(function() {
+	$("#projectDisplayModal button.projectsColumn").click(function() {
+		var isVisible = $(this).attr("data-project-visible");
+		if ("true" === isVisible) {
+			$(this).attr("data-project-visible", "false");
+			$.ajax({
+				type: 'GET',  
+				url: '/toggleProjectInvis/',
+				contentType: "application/json; charset=utf-8",
+				data: {projectID: $(this).attr("data-project")},
+				success: function(data) {},
+				failure: function(error) {
+					console.log("Project Display AJAX error: " + error);
+					$(this).attr("data-project-visible", "true");
+				}
+			});
+		} else {
+			$(this).attr("data-project-visible", "true");
+			$.ajax({
+				type: 'GET',  
+				url: '/toggleProjectInvis/',
+				contentType: "application/json; charset=utf-8",
+				data: {projectID: $(this).attr("data-project")},
+				success: function(data) {},
+				failure: function(error) {
+					console.log("Project Display AJAX error: " + error);
+					$(this).attr("data-project-visible", "false");
+				}
+			});
+		}
+	});
+});
