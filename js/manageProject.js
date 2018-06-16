986f08b2894c831581742a91fe91c3830ddd4de8
Kevin Zeng <kz571@ustart.today>
2018-06-16 17:50:50 -0400

    Projects

--- js/manageProject.js
+++ js/manageProject.js
@@ -1,5 +1,5 @@
     
-
+var port = location.port;
 function makeProjectApplications(name, avatar,docID, projectID, link){
         $('#request-groups').append('<li class="request-group" id="'+docID+'" ><div class="input-group-btn parentID" id='+projectID+' style="float:right; width:auto;"><button name="accept" class="btn btn-default y-btn" id="accept'+docID+'" type="submit"><i class="glyphicon glyphicon-ok" style="color:#607d8b;"></i></button><button class="btn btn-default x-btn" id="reject'+docID+'" name="reject" type="submit"><i class="glyphicon glyphicon-remove" style="color:#607d8b;"></i></button></div><a href="/profile/'+link+'"><img class="inbox-icon" id="avatar'+docID+'" src="" /><span class="group-header">'+ name +' </span></a><br><span class="invitation-message">would like to join your project! </span></li>');
         $("#avatar"+docID).attr("src", avatar);
