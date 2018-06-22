cd01d4208734c1bd77c6373a10b79d150c386eb5
Kevin Zeng <garudyne@gmail.com>
2018-06-22 17:53:47 -0400

    Recruit Member Template

--- js/project-nil.js
+++ js/project-nil.js
@@ -410,6 +410,9 @@ $(document).ready(function () {
     $("img#head-image").on('error', function () {
         $(this).attr('src', "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=");
     });
+
+    
+
     $('[data-toggle="tooltip"]').tooltip();
     //$(window).resize(function() {
     //  fitDashboard();
