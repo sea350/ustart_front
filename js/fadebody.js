de96d7fabbcdcc7cd87f32bfc88ec71639199016
Kevin Zeng <garudyne@gmail.com>
2018-07-21 19:07:56 -0400

    Remove Fade

--- js/fadebody.js
+++
@@ -1,8 +0,0 @@
-// Fade in effect on the document body to hide load jittering
-// Requires body to have display:none
-
-$('body').css('display', 'none');
-
-$(document).ready(function() {
-	$('body').delay('fast').fadeIn('fast');
-});
