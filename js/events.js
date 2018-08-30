64d9a6fd93b79e0f22db38523e2f6618abaaab5e
Kevin Zeng <garudyne@gmail.com>
2018-08-30 14:22:02 -0400

    Autocomplete Prep

--- js/events.js
+++ js/events.js
@@ -2,55 +2,6 @@ var editor_event = true; // value is true if the user can edit the event
 var followedIcon = '<i class="fa fa-check"></i><i class="fa fa-user-o"></i>'; //experimental icon
 var firstload = true;
 
-function loadMembers(userHandle) {
-    // Load the user's avatar image from the paramter userHandle.
-    var masterElement = $('.member').first().clone();
-    // This will direct the user to the member's profile page.
-    $(masterElement).attr('href', "profile.html#" + userHandle);
-    // Avatar image replaces the URL. Use 'userhandle' to find avatar and stats.
-    $(masterElement).children('img').attr('src', "https://www.fillmurray.com/20" + Math.floor(Math.random() * 10) + "/20" + Math.floor(Math.random() * 10));
-    // Name goes here. Might utilize IRL name.
-    $(masterElement).children('.member-name').text(randomText() + " " + randomText());
-
-    // Filler attributes
-    $(masterElement).children('.member-title').text("Developer");
-    $(masterElement).children('.member-followers').text(Math.floor(Math.random() * 800) + " Followers");
-    $(masterElement).children('.member-events').text(Math.floor(Math.random() * 30) + " Events");
-
-    $("#event-team").append($(masterElement));
-
-    $(masterElement).hover(function() {
-        //$(this).children('.member-name').slideDown('fast');
-        $(this).children('.member-title').slideDown('fast');
-        $(this).children('.member-followers').slideDown('fast');
-        $(this).children('.member-events').slideDown('fast');
-    }, function() {
-        //$(this).children('.member-name').delay('slow').slideUp('fast');
-        $(this).children('.member-title').delay('slow').slideUp('fast');
-        $(this).children('.member-followers').delay('slow').slideUp('fast');
-        $(this).children('.member-events').delay('slow').slideUp('fast');
-    });
-}
-
-
-function randomText() {
-    var text = "";
-    var alphabet = "abcdefghijklmnoprstu";
-    var vowels = "aeoiu";
-
-    text += alphabet.charAt(Math.floor(Math.random() * alphabet.length)).toUpperCase();
-
-    for (var i = 0; i < 5; i++) {
-        var temp = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-        while (vowels.includes(text.substr(text.length - 1, text.length)) && vowels.includes(temp) || !vowels.includes(text.substr(text.length - 1, text.length)) && !vowels.includes(temp)) {
-            temp = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-        }
-        text += temp;
-    }
-
-    return text;
-}
-
 function fitDashboard() {
     var widthLength = 500;
     var scrollFlag = $(document).height() > $(window).height();
@@ -137,60 +88,26 @@ $(document).ready(function() {
             $button.html('Follow').css('width', '8em');
 		}
     });
-	$('#btn-message').click(function() {
-		window.location.href = "inbox.html";
-	});
-
-    // Load members of the event.
-    for (i = 1; i < 0; i++) {
-        loadMembers(i);
-    }
-
-    $('.member').hover(function() {
-        //$(this).children('.member-name').stop(true, false).slideDown('fast');
-        $(this).children('.member-title').stop(true, false).slideDown('fast');
-        $(this).children('.member-followers').stop(true, false).slideDown('fast');
-        $(this).children('.member-events').stop(true, false).slideDown('fast');
-    }, function() {
-        //$(this).children('.member-name').delay('slow').slideUp('fast');
-        $(this).children('.member-title').delay('slow').slideUp('fast');
-        $(this).children('.member-followers').delay('slow').slideUp('fast');
-        $(this).children('.member-events').delay('slow').slideUp('fast');
-    });
 	
     $(window).resize(function() {
 		fitDashboard();
     });
-   // fitDashboard();
-	
-	$('#eventFollowerModal').on('shown.bs.modal', function() {
-		// Generate followers
-		if (firstload) {
-			populateFollowerList(791);
-			populateFollowerList(792);
-			populateFollowerList(800);
-			for (i = 0; i < 17; i++) {
-				populateFollowerList(Math.floor((Math.random() * 802) + 1));
-			}
-			firstload = false;
-		}
-	});
+	fitDashboard();
+
+	addingAutoComplete();
 });
 
-function populateFollowerList(num) {
-	var generateurl = "https://cors.now.sh/http://pokeapi.co/api/v2/pokemon/" + num;
-	$.getJSON(generateurl, function( data ) {
-		followerCallback(data);
+function addingAutoComplete() {
+	$("#eventMemberName").autocomplete({
+		source: "/FindEventMember/",
+		minLength: 3,
 	});
-}
-
-function followerCallback(data) {
-	var followerUsername = data.name;
-	var followerProfilePic = data.sprites.front_default;
-	var followerTag = '<li><a href="profile.html#' + followerUsername + '"><img src="' + followerProfilePic + '"/><div class="event-modal-followers-name">' + followerUsername + '</div></a></li>';
-	var followerElem = $(followerTag + " ").appendTo($('#event-modal-followers-list'));
-	followerElem.hide();
-	followerElem.find('img').on('load', function(event) {
-		$(this).parents('li').fadeIn('slow');
+	$("#eventProjectName").autocomplete({
+		source: "/FindEventProject/",
+		minLength: 3,
+	});
+	$("#eventGuestName").autocomplete({
+		source: "/FindEventGuest/",
+		minLength: 3,
 	});
 }
