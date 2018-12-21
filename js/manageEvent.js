394028fc079c42827d6bd7863c044e376018bd52
dannyhu95 <yh1112@nyu.edu>
2018-12-21 12:33:02 -0500

    google api

--- js/manageEvent.js
+++ js/manageEvent.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
 		   type: 'GET',
 		   url: 'http://k12start.today:'+port+'/AcceptGuestJoinRequest/',
 		   contentType: "application/json; charset=utf-8",
-		   data: {userID:usrID, projectID: projID},
+		   data: {userID:usrID, eventID: eventID},
 		   success: function(result) {
 			   //update notification
                console.log(result);
